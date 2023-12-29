c2fa7bba665115ab45555012e1882986a677feca
augustine-collabNow <chineduezeh19@gmail.com>
2023-12-28 22:22:00 -0800

    chore: created initial form

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Simple Login Form</Text>
+      <TextInput
+      placeholder='email'
+      style={styles.input}
+        />
+      <TextInput
+      placeholder='full name'
+      style={styles.input}
+        />
+      <TextInput
+      placeholder='password'
+      style={styles.input}
+      secureTextEntry
+        />
+
+      <Button
+      title='Submit'
+      />
+
     </View>
   );
 }
@@ -14,7 +31,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+
+    gap: 10
   },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: "#000",
+    width: '100%',
+    textAlign: 'left',
+    padding: 10,
+    borderRadius: 10
+  }
 });
